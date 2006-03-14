baf20d719f3a820b81b9ca0ef74b4fc60e14dd26
kuhn <kuhn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2006-03-14 16:52:41 +0000

    removed digits argument
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/odfweave@9 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/odfTable.character.R
+++ pkg/R/odfTable.character.R
@@ -3,14 +3,13 @@ function(
    x, 
    horizontal = length(x) > 5,
    colnames    = names(x),  # allow alt col names (what about dimnames[[1]]?)
-   digits      = max(3, getOption("digits") - 3), 
    name = paste("Table", floor(runif(1) * 1000), sep = ""),
    styleNames = NULL,
    ...)
 {
    xMat <- if(horizontal) as.matrix(t(x)) else as.matrix(x)
    colTypes <- apply(xMat, 2, odfDataType)   
-   xChar <- format(xMat, digits = digits, ...)
+   xChar <- format(xMat, ...)
    tbleText <- odfTableGen(xChar, colTypes, header = colnames, tableName = name, styles = styleNames)
    tbleText 
 }
