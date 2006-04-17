9fa5d7b0a7449e88f1fc516e7ee854a33a1fd5a6
jainn02 <jainn02@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2006-04-17 21:47:46 +0000

    Added a new verion of getExt, which is more robust and fixes few errors
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/odfweave@26 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/getExt.R
+++ pkg/R/getExt.R
@@ -13,5 +13,36 @@ function(x)
    unlist(z)
 }
 
+"getExt.new" <-
+function(x)
+{
+   hasPeriod <-  grep("\\.", x)
+   if(length(hasPeriod) == 0) {
+     z <- rep(NA, length(x))
+   } else {
+     if(length(hasPeriod) < length(x))
+       x[-hasPeriod] <- NA
+     y <- strsplit(x, ".", extend = FALSE, fixed = TRUE)
+     z <- lapply(y, function(data)
+                 { if (length(data) > 1)
+                     {
+                       data[length(data)]
+                     } else NA
+                 }
+                 )
+   }
+   unlist(z)
+}
+
+
+## Test it - last two examples work only for the new version
+## getExt.new(c("a.txt", "b.R"))
+## getExt.new("a.txt")
+## getExt.new("a")
+## getExt.new(c("a.txt", "b"))
+## getExt.new(c("a", "b"))
+## getExt.new(c("a.txt", "b."))
+
+
 
 
