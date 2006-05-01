d0f9a006022c4abc18c283c923c79dcfdc73bdb9
kuhn <kuhn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2006-05-01 14:13:00 +0000

    Added some error trapping
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/odfweave@32 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/stripXmlTag.R
+++ pkg/R/stripXmlTag.R
@@ -1,5 +1,7 @@
 stripXmlTag <- function(x, leadWhite = FALSE, endWhite = FALSE)
 {
+   if(!is.character(x)) stop("x must be character")
+   if(!is.vector(x)) stop("x must be a vector")
    # annihilate anything between a < and a >
    x <- gsub("<[^>]*>", "", x)
    if(!leadWhite) x <- sub("^[ \t]+", "", x)  
