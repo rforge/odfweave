2342e28a298a2955f1bd3f442724673a4bc49044
kuhn <kuhn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2006-05-17 18:20:01 +0000

    changed to return a vector instead of a string. Also removed class
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/odfweave@48 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/odfInsertPlot.R
+++ pkg/R/odfInsertPlot.R
@@ -1,48 +1,31 @@
 "odfInsertPlot" <-
 function(file, height, width, 
    units = "in",
-   anchor = c("\n<text:p>\n", "\n</text:p>\n"),
+   anchor = c("<text:p>", "</text:p>"),
    name = paste("graphics", floor(runif(1) * 1000), sep = ""), 
-   copy = TRUE, 
-   dest = paste(getwd(), "/Pictures", sep = ""),
    verbose = FALSE)
 {
 
    if(!(getExt(file) %in% c("png", "jpeg", "jpg", "gif", "tiff", "bmp")))
       stop("graphics format not supported")
-
-   if(copy)
-   {
-      newPath <- paste(dest, "/", basename(file), sep = "")
+ 
    
-      if(!file.exists(dest))
-         stop(paste(dest, "does not exist"))   
-      if(verbose) cat("  Copying ", file, " to ", dest, "\n"); flush.console()      
-      if(!file.copy(file, newPath,  overwrite = TRUE))
-         stop("Error copying file")
-   }
-   
-   
-   plotString <- paste(
+   plotString <- c(
       "    <draw:frame ",
-      "\n",
-#      "     draw:style-name=\"fr1\"\n",
-      "      draw:name=\"", name, "\"\n",
-      "      text:anchor-type=\"paragraph\"\n",
-      "      svg:width=\"", width, units, "\"\n",
-      "      svg:height=\"", height, units, "\"\n",
-      "      draw:z-index=\"0\">\n",
-      "      <draw:image\n",
-      "         xlink:href=\"Pictures/", basename(file), "\"\n",
-      "         xlink:type=\"simple\"\n",
-      "         xlink:show=\"embed\"\n",
-      "         xlink:actuate=\"onLoad\"/>\n",
-      "    </draw:frame>",
-      sep = "")
+      paste("      draw:name=\"", name, "\"", sep = ""),
+      "      text:anchor-type=\"paragraph\"",
+      paste("      svg:width=\"", width, units, "\"", sep = ""),
+      paste("      svg:height=\"", height, units, "\"", sep = ""),
+      "      draw:z-index=\"0\">",
+      "      <draw:image",
+      paste("         xlink:href=\"Pictures/", basename(file), "\"", sep = ""),
+      "         xlink:type=\"simple\"",
+      "         xlink:show=\"embed\"",
+      "         xlink:actuate=\"onLoad\"/>",
+      "    </draw:frame>")
       
-   out <- paste(anchor[1], plotString, anchor[2])   
-   structure(out, class = "odfInsertPlot")     
+   out <- c(anchor[1], plotString, anchor[2])   
+   out   
 }
 
-print.odfInsertPlot <- function(x, ...) cat(x)
 
