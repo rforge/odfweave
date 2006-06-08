76f31b61b372dab34cabc7fc03f1207135443e01
kuhn <kuhn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2006-06-08 20:04:33 +0000

    added option to go from r to XML
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/odfweave@65 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/odfTranslate.R
+++ pkg/R/odfTranslate.R
@@ -1,13 +1,22 @@
 "odfTranslate" <-
-function(x)
+function(x, toR = TRUE)
 {
-   x <- gsub("&gt;", ">", x)
-   x <- gsub("&lt;", "<", x)
-   x <- gsub("&quot;", "\"", x)
-   x <- gsub("&apos;", "\'", x)
-   x <- gsub("&amp;", "&", x)  
-   x <- gsub("\342\200\235", "\"", x)  
-   x <- gsub("\342\200\234", "\"", x)        
+   if(toR)
+   {
+      x <- gsub("&gt;", ">", x)
+      x <- gsub("&lt;", "<", x)
+      x <- gsub("&quot;", "\"", x)
+      x <- gsub("&apos;", "\'", x)
+      x <- gsub("&amp;", "&", x)  
+      x <- gsub("\342\200\235", "\"", x)  
+      x <- gsub("\342\200\234", "\"", x)        
+   } else {
+      x <- gsub("&","&amp;",  x)       
+      x <- gsub(">", "&gt;", x)
+      x <- gsub("<", "&lt;",  x)
+      x <- gsub("\"", "&quot;",  x)
+      x <- gsub( "\'", "&apos;", x)
+   }
    x
 }
 
