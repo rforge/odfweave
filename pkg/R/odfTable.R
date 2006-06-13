2375583dda25cc03d5011e5aca9e1d8c4ac2eb86
kuhn <kuhn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2006-06-13 20:01:14 +0000

    revised print method to work with new structure
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/odfweave@90 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/odfTable.R
+++ pkg/R/odfTable.R
@@ -4,5 +4,11 @@ function(x, ...)
    UseMethod("odfTable")
 }
 
-print.odfTable <- function(x, ...) cat(x)
+print.odfTable <- function(x, ...)
+{
+   cat(x$start)
+   if(!is.null(x$header)) cat(x$header)
+   if(any(dim(x$cells) == 1)) cat(x$cells) else cat(t(x$cells))
+   cat(x$end)
+}
 
