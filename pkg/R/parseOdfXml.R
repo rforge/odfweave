a9b1142788b9d988651698ca6dc0139c180853d5
kuhn <kuhn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2006-06-13 13:13:40 +0000

    removed Rtangle and figure dection; bug fixes
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/odfweave@80 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/parseOdfXml.R
+++ pkg/R/parseOdfXml.R
@@ -4,72 +4,67 @@
 # (hopefully) consistent with Sweave's expectations.
 
 "parseOdfXml" <-
-function(x, control)
-{
-   leadWhite <- function(x) sub ("^[ \t]*", "", sub ("[ \t]*$", "", x)) 
-     
-   xml2R <- function(x)
-   {
-      if(length(grep("<text:line-break/>", x)) >= 1) x <- unlist(strsplit(x, "<text:line-break/>"))  
-      tmpString <- stripXmlTag(x)
-      tmp <- odfTranslate(tmpString)            
-      tmp
-   }
+function(x)
+{   
    
    # for in-line Sweave tags, make sure that any converted character
    # are fixed prior to the Sweave call
    SexpLines <- grep("\\Sexpr\\{([^\\}]*)\\}", x)
-   x[SexpLines] <- odfTranslate(x[SexpLines])
+   for(i in SexpLines) x[i] <- processInLine(x[i])
+   
+   # process lines with options. In this case, we will 
+   # nuke everything else on the line
 
+   SoptLines <- grep("\\SweaveOpts\\{([^\\}]*)\\}", x)
+   x[SoptLines] <- odfTranslate(stripXmlTag(x[SoptLines]))
+     
    # search thru for lines with <<>>=
    startLines <- grep("&lt;&lt;(.*)&gt;&gt;=", x)
-
-   hasPlots <- isPlotChunk(x[startLines])
-   
-
-   for(i in seq(along = startLines))
+   if(length(startLines) > 0)
    {
-      #rnwOut is the test of the file that Sweave will run on
-      if(i == 1) rnwOut <- x[1:(startLines[1] - 1)]
-      
-      # create a container for the pure R code
-      pureR <- vector(mode = "character", length = 0)
-      
-      # count will contain the number of lines of xml that
-      # contain R code
-      count <- 0
-          
-      # scan the xml until the end of the code chunk (a line with only a @)
-      for(j in startLines[i]:length(x))
+      for(i in seq(along = startLines))
       {
-      
-         count <- count + 1
+         #rnwOut is the test of the file that Sweave will run on
+         if(i == 1) rnwOut <- x[1:(startLines[1] - 1)]
          
-         # convert the line of xml to pure R and possible
-         # expand the number of lines of pure R, i.e, 
-         # length(x) may !=  length(xml2R(x[j))         
-         rCode <- xml2R(x[j])
+         # create a container for the pure R code
+         pureR <- vector(mode = "character", length = 0)
          
-         # append this/these lines of R to the rest of
-         # the code chunk
-         pureR <- c(pureR, rCode)
+         # count will contain the number of lines of xml that
+         # contain R code
+         count <- 0
+             
+         # scan the xml until the end of the code chunk (a line with only a @)
+         for(j in startLines[i]:length(x))
+         {
          
-         if(length(grep("^@", rCode)) > 0)
-         {         
-            # append R chunk to existing xml
-            rnwOut <- c(rnwOut, pureR)
+            count <- count + 1
             
-            # if there is more xml between the end of the chunk
-            # and the next chunk, add it
-            if(i < length(startLines))
-            {
-               rnwOut <- c(rnwOut, x[(startLines[i] + count):(startLines[i + 1] - 1)])
-            } else rnwOut <- c(rnwOut, x[(startLines[i] + count):length(x)])
-            break            
+            # convert the line of xml to pure R and possible
+            # expand the number of lines of pure R, i.e, 
+            # length(x) may !=  length(xml2R(x[j))         
+            rCode <- xml2R(x[j])
+            
+            # append this/these lines of R to the rest of
+            # the code chunk
+            pureR <- c(pureR, rCode)
+            
+            if(length(grep("^@", rCode)) > 0)
+            {         
+               # append R chunk to existing xml
+               rnwOut <- c(rnwOut, pureR)
+               
+               # if there is more xml between the end of the chunk
+               # and the next chunk, add it
+               if(i < length(startLines))
+               {
+                  rnwOut <- c(rnwOut, x[(startLines[i] + count):(startLines[i + 1] - 1)])
+               } else rnwOut <- c(rnwOut, x[(startLines[i] + count):length(x)])
+               break            
+            }
          }
       }
-   }
-         
+   } else rnwOut <- x      
    rnwOut
 }
 
