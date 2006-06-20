095ddb6cfd2e61b686b73e079575c222d62a515d
kuhn <kuhn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2006-06-20 20:44:47 +0000

    almost complete rewrite
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/odfweave@99 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/odfTableGen.R
+++ pkg/R/odfTableGen.R
@@ -1,67 +1,38 @@
 "odfTableGen" <-
-function(x, dataType, header = NULL, tableName, styles = NULL)
+function(x, dataType, header = NULL, tableName, styles)
 {
+
    tableDim <- dim(x)
    
-   if(!is.null(styles))
-   {
-      if(!is.null(styles$table)) tableStyle <- paste(" table:style-name=\"", styles$table, "\" ", sep = "")
-         else tableStyle <- ""
-      if(!is.null(styles$tableColumn)) tableColumnStyle <- paste(" table:style-name=\"", styles$tableColumn, "\" ", sep = "")
-         else tableColumnStyle <- ""
-      if(!is.null(styles$tableRow)) tableRowStyle <- paste(" table:style-name=\"", styles$tableRow, "\" ", sep = "")
-         else tableRowStyle <- ""
-      if(!is.null(styles$tableCell)) tableCellStyle <- paste(" table:style-name=\"", styles$tableCell, "\" ", sep = "")
-         else tableCellStyle <- ""
-      if(!is.null(styles$cellText)) textStyle <- paste(" text:style-name=\"", styles$cellText, "\" ", sep = "")
-         else textStyle <- ""
-      if(!is.null(styles$cellHeaderText)) textHeaderStyle <- paste(" text:style-name=\"", styles$cellHeaderText, "\" ", sep = "")
-         else textHeaderStyle <- ""      
-   } else tableStyle <- tableColumnStyle <- tableRowStyle <- tableCellStyle <- textStyle <- textHeaderStyle <- ""
+   has <- function(x) !is.null(x) && x != ""
+   makeMatrix <- function(text, dims) matrix(rep(text,prod(dims)),nrow = dims[1])
    
-   
-   if(!is.null(header))
-   {
-      headLine01 <- paste("\n      <text:p ", textHeaderStyle, "> ", header, " </text:p>", sep = "")
+   endQuote <- makeMatrix("\" ", dim(styles$cell))
+  
+    
+   # build text tags and formatting 
+   textNameStart <- makeMatrix(" text:style-name=\"", dim(styles$text))
+   textNameStart <- ifelse(styles$text == "", "", textNameStart)
+   textNameEnd <- ifelse(styles$text == "", "", endQuote)   
+   textName <- matrixPaste(textNameStart, styles$text, textNameEnd, sep = c("", ""))
+   textStart <- makeMatrix("      <text:p", dim(styles$text))
+   textEnd <- makeMatrix(">", dim(styles$text))
+   tagEnd <- makeMatrix(" </text:p>\n", dim(styles$text))   
+   textMatrix <- matrixPaste(textStart, textName, textEnd, x, tagEnd)
 
-      headLine02 <- paste(
-         "\n    <table:table-cell ", 
-         tableCellStyle, 
-         "office:value-type=\"string\">", 
-         headLine01, 
-         "\n    </table:table-cell>", 
-         sep = "")
+   # cell properties
+   cellNameStart <- makeMatrix(" table:style-name=\"", dim(styles$cell))
+   cellNameStart <- ifelse(styles$cell == "", "", cellNameStart)
+   cellNameEnd <- ifelse(styles$cell == "", "", endQuote)   
+   cellName <- matrixPaste(cellNameStart, styles$cell, cellNameEnd, sep = c("", ""))
+   valueType <- matrix(rep(dataType, each = tableDim[1]), nrow = tableDim[1])
+   valueTypeStart <- makeMatrix(" <table:table-cell office:value-type=\"", dim(styles$cell))
+   cellStart <- matrixPaste(valueTypeStart, valueType, endQuote, sep = c("", ""))
+   cellEnd <- makeMatrix(">\n", dim(styles$cell))
+   tagEnd <- makeMatrix(" </table:table-cell>\n", dim(styles$cell))   
+   cellMatrix <- matrixPaste(cellStart, cellName, cellEnd, textMatrix, tagEnd)         
 
-      headLine03 <- paste(
-         "\n  <table:table-header-rows>\n    <table:table-row>\n",
-         paste(headLine02, collapse = ""),
-         "\n    </table:table-row>\n   </table:table-header-rows>\n",
-         sep = "")
-
-   } else headLine03 <-  NULL   
-   
-   
-   leftValueTags <- matrix(
-      rep(
-         paste("      <text:p ",  textStyle, "> ", sep = ""),
-         prod(tableDim)),
-      nrow = tableDim[1])
-   rightValueTags <- matrix(
-      rep(
-         paste(" </text:p>", sep = ""),
-         prod(tableDim)),
-      nrow = tableDim[1])      
-   valueMarkup <- matrixPaste(leftValueTags, x, rightValueTags,  sep = rep("", 3))
-   columnMarkup <- rep(
-         paste("    <table:table-cell ", tableCellStyle, "office:value-type=\"", dataType, "\">", sep = ""),
-         tableDim[1])
-   leftCellTags <- matrix(columnMarkup, nrow = tableDim[1], byrow = TRUE)      
-   rightCellTags <- matrix(
-      rep(
-         paste(" </table:table-cell>", sep = ""),
-         prod(tableDim)),
-      nrow = tableDim[1])    
-   cellMarkup <- matrixPaste(leftCellTags, valueMarkup, rightCellTags, sep = rep("\n", 3))
+   # wrap in row tags
    leftRowTags <- matrix(
       c(
          rep("<table:table-row>", tableDim[1]),
@@ -72,11 +43,39 @@ function(x, dataType, header = NULL, tableName, styles = NULL)
          rep("", (tableDim[2] - 1) * tableDim[1]),      
          rep("</table:table-row>\n", tableDim[1])),
       nrow = tableDim[1])   
-   rowMarkup <- matrixPaste(leftRowTags, cellMarkup, rightRowTags, sep = rep("\n", 3))
+   rowMarkup <- matrixPaste(leftRowTags, cellMatrix, rightRowTags, sep = rep("\n", 2))
+
+   if(has(styles$table)) tableStyle <- paste(" table:style-name=\"", styles$table, "\" ", sep = "") else tableStyle <- ""
+ 
+   if(!is.null(header))
+   {
+      cellHeaderStyle <- paste(" table:style-name=\"", styles$headerCell, "\" ", sep = "")
+      cellHeaderStyle <- ifelse(styles$headerCell == "", "", cellHeaderStyle)    
+   
+   
+      textHeaderStyle <- paste(" text:style-name=\"", styles$header, "\" ", sep = "")
+      textHeaderStyle <- ifelse(styles$header == "", "", textHeaderStyle)           
       
+      headLine01 <- paste("\n      <text:p ", textHeaderStyle, "> ", header, " </text:p>", sep = "")
+      headLine02 <- paste(
+         "\n    <table:table-cell ", 
+         cellHeaderStyle, 
+         "office:value-type=\"string\">", 
+         headLine01, 
+         "\n    </table:table-cell>", 
+         sep = "")
+      headLine03 <- paste(
+         "\n  <table:table-header-rows>\n    <table:table-row>\n",
+         paste(headLine02, collapse = ""),
+         "\n    </table:table-row>\n   </table:table-header-rows>\n",
+         sep = "")
+
+   } else headLine03 <-  NULL   
+   
+   
    startText <- paste(
       "\n<table:table table:name=\"",  tableName, "\" ", tableStyle, ">", 
-      "\n  <table:table-column ",  tableColumnStyle, 
+      "\n  <table:table-column ",   
       "table:number-columns-repeated=\"", length(dataType), "\"/>",
       sep = "")
 
