ea28e233359cd775e96db69bf1782bbb05c28aa6
kuhn <kuhn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2006-06-20 20:48:37 +0000

    new parameter to send xml to content.xml
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/odfweave@105 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/addStyleDefs.R
+++ pkg/R/addStyleDefs.R
@@ -1,12 +1,14 @@
-addStyleDefs <- function(x, style, verbose = TRUE)
+addStyleDefs <- function(x, style, dest = "styles", verbose = TRUE)
 {
    # we need to add our style info between <office:styles> tags
    # which may not exist
-   if(verbose) cat("  Looking for style information in styles.xml\n"); flush.console()           
-   locateAutoStart <- grep("<office:styles>", x, fixed = TRUE)
-   locateAutoStop <- grep("</office:styles>", x, fixed = TRUE)
+   startTag <- if(dest == "styles") "<office:styles>" else "<office:automatic-styles>"
+   endTag <- gsub("<", "</", startTag)
+   if(verbose) cat(paste("\n  Looking for style information in " , dest, ".xml\n", sep = "")); flush.console()           
+   locateAutoStart <- grep(startTag, x, fixed = TRUE)
+   locateAutoStop <- grep(endTag, x, fixed = TRUE)
    if(length(locateAutoStart) > 1 & length(locateAutoStop) > 1)
-      stop("there are more that one <office:styles> or <office:styles> tags")
+      stop(paste("there are more that one ", startTag, "tags"))
       
    # for some files, there may only be the closing style tag
    # we need to create the opening tag and note the locations   
@@ -14,14 +16,14 @@ addStyleDefs <- function(x, style, verbose = TRUE)
    {
       part1 <- x[1:(locateAutoStop - 1)]
       part2 <- x[locateAutoStop:length(x)]
-      x <- c(part1, " <office:styles>", part2)
+      x <- c(part1, startTag, part2)
       locateAutoStart <- locateAutoStop
       locateAutoStop <- locateAutoStop + 1     
    } 
-   if(verbose) cat("  Splitting file around <office:styles> at line", locateAutoStop, "\n"); flush.console()           
-   part1 <- x[1:(locateAutoStop -1)] # xml before <office:styles>
-   part2 <- x[locateAutoStop:length(x)] # xml after </office:styles>
-   styleVec <- odfStyleGen(style)            
+   if(verbose) cat(paste("  Splitting file around", startTag, "at line", locateAutoStop, "\n")); flush.console()           
+   part1 <- x[1:(locateAutoStop -1)] # xml before tag
+   part2 <- x[locateAutoStop:length(x)] # xml after tag
+   styleVec <- odfStyleGen(style, type = dest)            
    if(verbose) cat("\n")
   
    c(paste(part1, "\n"), styleVec, paste(part2, "\n"))   
