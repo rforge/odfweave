a082c845a99666f1ff62dddf39204d00c9a1c1ff
kuhn <kuhn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2006-08-13 00:54:41 +0000

    rolled back to correct version
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/odfweave@159 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/getStyles.R
+++ pkg/R/getStyles.R
@@ -5,7 +5,7 @@ setStyles <- function(style)
    styleNames <- names(style)
    required <- c("paragraph", "input", "output", "table", "cell", "header", "cellText", "headerText")
    hasTypes <- required %in% styleNames
-
+   
    if(any(!hasTypes))
    {
       missingList <- vector(mode = "list", length = sum(!hasTypes))
@@ -13,8 +13,8 @@ setStyles <- function(style)
       missingList <- lapply(missingList, function(x) "")
       style <- c(style, missingList)
    }
-
-#   if(any(!hasTypes))
+   
+#   if(any(!hasTypes)) 
 #      stop(
 #         cat("There must be styles for:",
 #            paste(required, collapse = ", "), "\n"))
@@ -31,11 +31,77 @@ setStyleDefs <- function(def)
    styleTypes <- unique(unlist(lapply(def, function(x) x$type)))
    required <- c("Paragraph", "Table Cell", "Table", "Bullet List")
    hasTypes <- required %in% styleTypes
-   if(any(!hasTypes))
+   if(any(!hasTypes)) 
       stop(
          cat("There must be at least one style definintion for:",
             paste(required, collapse = ", "), "\n"))
    assign( "styleDefs",  def, env = .odfEnv)
 }
 
+getImageDefs <- function() get("imageDefs", envir = .odfEnv)
+
+setImageDefs <- function(def, verbose = TRUE)
+{
+   if(!(all(names(defs) %in% c("type", "device", "plotHeight", 
+      "plotWidth", "dispHeight", "dispWidth", "args" ))))
+      stop("arguments were included. see ?setImageDefs")
+
+   if(!is.null(def$args))
+   {
+      if(!is.list(def$args)) stop("args must be a list")
+      if(any(names(def$args) %in% c("width", "height")))
+         stop("these options should be specified without using the args list")
+   }
+   
+  
+   if(def$device %in% c("pdf", "svg")) stop("pdf and svg formats not supported by OpenOffice")   
+   if(def$device %in% c("png", "bmp", "jpeg"))
+   {
+      if(def$device == "png" & !capabilities("png"))
+         stop("R png device not enabled")
+      if(def$device %in% c("bmp", "jpeg") & !capabilities("jpeg"))
+         stop(paste("R", def$device,"device not enabled"))      
+   }
+   
+  
+   if(def$device == "postscript" & verbose)
+   {
+      psArgs <- names(def$args) %in% c("horizontal", "onefile", "paper")
+      psNote <- paste(
+         "you will probabiliy need to set",
+         "\nhorizontal = FALSE, onefile = FALSE,",
+         "and paper = \"special\" to",
+         "generate ps graphics for OpenOffice\n")
+      if(length(psArgs) == 0 | any(!psArgs)) cat(psNote)
+   }
+   current <- getImageDefs()
+   for(i in names(current))
+   {
+      if(!is.null(def[[i]]) && current[[i]] != def[[i]]) current[[i]] <- def[[i]]
+   }
+   
+   if(current$device %in% c("png", "bmp", "jpeg") & verbose)
+   {
+      if(current$plotHeight <= 30 | current$plotWidth <= 30)
+         cat(
+            paste(
+               "an image size of",
+               current$plotHeight,
+               "pixels by",
+               current$plotWidth,
+               "pixels has been requested.\n"))   
+   } else {
+      if(current$plotHeight >= 30 | current$plotWidth >= 30)
+         cat(
+            paste(
+               "an image size of",
+               current$plotHeight,
+               "inches by",
+               current$plotWidth,
+               "inches has been requested.\n"))     
+   }
+   
+   flush.console()
+   assign("imageDefs",  current, env = .odfEnv)
+}
 
