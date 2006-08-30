7c4673d8fc59767f5c415e052dbe62551e17e418
kuhn <kuhn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2006-08-30 21:07:59 +0000

    removed addFontDecs and put in own fle
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/odfweave@195 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/addStyleDefs.R
+++ pkg/R/addStyleDefs.R
@@ -1,6 +1,3 @@
-control <- odfWeaveControl()
-verbose <- control$verbose
-
 "addStyleDefs" <- function(x, style, dest = "styles", verbose = TRUE) {
    #input
    #
@@ -11,7 +8,7 @@ verbose <- control$verbose
 
 
    # add font declarations
-   x <- addFontDecs(x)
+   x <- addFontDecs(x, verbose)
 
    # we need to add our style info between <office:styles> tags
    # which may not exist
@@ -44,55 +41,4 @@ verbose <- control$verbose
    out
 }
 
-"addFontDecs" <- function (content) {
-   #input
-   #
-   #   content:  character string, contents of a document
-   #
-   #value: character string, x with additional font declarations appended.
-   #    Fonts listed in odfEnv.R are checked and added, if necessary
-   #
-
-   announce(verbose, "adding font declarations...\n")
-
-   #get styles used in odfEnv
-   styles <- getStyleDefs()
-   #extract font names from styles
-   fonts <- unique(unlist(lapply(styles, function(font) font$fontName)))
-
-   #get font declarations
-   #assert:  content only has one such font-face-decls element
-   match <- regexpr(
-      "(?s)(?U)<office:font-face-decls>.*</office:font-face-decls>",
-      content, perl=TRUE)
-   matchLength <- attr(match, "match.length")
-   fontDecBlock <- substr(content, match, match + matchLength -1)
-
-   #get only fonts which don't have a declaration
-   fonts <-fonts[!unlist(lapply(lapply(fonts, grep, fontDecBlock), any))]
-
-   #create a string of font declarations to insert
-   fontDecs <- paste(
-      unlist(
-         lapply(fonts,
-            function(font){
-               element(
-                  "style:font-face",
-                  c(
-                     tagattr("style:name", font),
-                     tagattr("svg:font-family", font)
-                  )
-               )
-            }
-         )
-      ),
-      collapse=""
-   )
-   #insert font declarations
-   sub("</office:font-face-decls>",
-      paste(fontDecs, "</office:font-face-decls>", sep=""),
-      content,
-      fixed=TRUE
-   )
-}
 
