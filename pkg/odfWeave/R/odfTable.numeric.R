0258fd0a3cc7fc51154b922472d00803c107e1dc
kuhn <kuhn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-03-18 20:25:38 +0000

    copied R to odfWeave
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/odfweave@263 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/odfWeave/R/odfTable.numeric.R
@@ -0,0 +1,21 @@
+"odfTable.numeric" <-
+function(
+   x,
+   horizontal = length(x) < 5,
+   colnames    = names(x),  # allow alt col names (what about dimnames[[1]]?)
+   digits      = max(3, getOption("digits") - 3),
+   name = paste("Table", floor(runif(1) * 1000), sep = ""),
+   styles = NULL,
+   ...)
+{
+   if(!is.null(colnames)) colnames <- odfTranslate(colnames, toR = FALSE)  
+   xMat <- if(horizontal) as.matrix(t(x)) else as.matrix(x)
+   colTypes <- apply(xMat, 2, odfDataType)
+   xChar <- format(xMat, digits = digits, ...)
+
+   if(is.null(styles))    styles <- tableStyles(xChar, useRowNames = FALSE, colnames)
+
+   tbleText <- odfTableGen(xChar, colTypes, header = colnames, tableName = name, styles)
+   structure(tbleText, class = "odfTable")
+}
+
